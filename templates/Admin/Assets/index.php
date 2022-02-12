aa206acf43cab51eaf3edcbf5f0b26fcc06e8a77
passchn <77938819+passchn@users.noreply.github.com>
2022-02-12 01:54:54 +0100

    add limit control

--- templates/Admin/Assets/index.php
+++ templates/Admin/Assets/index.php
@@ -74,6 +74,7 @@
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
+        <p><?= $this->Paginator->limitControl() ?></p>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
