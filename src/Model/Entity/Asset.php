b81771d981e507d68af624ece3bb6cf8f783b2e6
Pascal Schneider <psc@brandcom.de>
2022-11-21 13:47:34 +0100

    mimetype can be null

--- src/Model/Entity/Asset.php
+++ src/Model/Entity/Asset.php
@@ -22,7 +22,7 @@ use Nette\Utils\Strings;
  * @property string|null $category
  * @property string $filename
  * @property string $directory
- * @property string $mimetype
+ * @property string|null $mimetype
  * @property string $filesize
  * @property string $filetype
  * @property string $absolute_path
@@ -91,17 +91,17 @@ class Asset extends Entity
         return Arrays::contains([
                 'image',
                 'video'
-            ], Strings::before($this->mimetype, '/'))
+            ], Strings::before((string)$this->mimetype, '/'))
             || Arrays::contains([
                 'pdf',
                 'json'
-            ], Strings::after($this->mimetype, '/'));
+            ], Strings::after((string)$this->mimetype, '/'));
     }
 
     public function isImage(): bool
     {
         return $this->exists()
-            && Strings::before($this->mimetype, '/') === 'image'
+            && Strings::before((string)$this->mimetype, '/') === 'image'
             && !Strings::contains((string)Strings::after($this->mimetype, '/'), 'svg');
     }
 
