d9d0244e0ba2416a406552e9ac01bb66fbcf2471
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2022-11-23 22:15:13 +0100

    use __d() translate function and generate assets.pot file

--- templates/Admin/Assets/edit.php
+++ templates/Admin/Assets/edit.php
@@ -7,20 +7,20 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __d('assets','Actions') ?></h4>
+            <h4 class="heading"><?= __d('assets', 'Actions') ?></h4>
             <?= $this->Form->postLink(
-                __d('assets','Delete'),
+                __d('assets', 'Delete'),
                 ['action' => 'delete', $asset->id],
-                ['confirm' => __d('assets','Are you sure you want to delete # {0}?', $asset->id), 'class' => 'side-nav-item']
+                ['confirm' => __d('assets', 'Are you sure you want to delete # {0}?', $asset->id), 'class' => 'side-nav-item']
             ) ?>
-            <?= $this->Html->link(__d('assets','List Assets'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__d('assets', 'List Assets'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="assets form content">
             <?= $this->Form->create($asset, ['type' => 'file']) ?>
             <fieldset>
-                <legend><?= __d('assets','Edit Asset') ?></legend>
+                <legend><?= __d('assets', 'Edit Asset') ?></legend>
                 <?= $asset->getThumbnail() ?>
                 <?php
                     echo $this->Form->control('title');
@@ -30,7 +30,7 @@
                 ?>
 
             </fieldset>
-            <?= $this->Form->button(__d('assets','Submit')) ?>
+            <?= $this->Form->button(__d('assets', 'Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
--- templates/Admin/Assets/index.php
+++ templates/Admin/Assets/index.php
@@ -5,8 +5,8 @@
  */
 ?>
 <div class="assets index content">
-    <?= $this->Html->link(__d('assets','New Asset'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __d('assets','Assets') ?></h3>
+    <?= $this->Html->link(__d('assets', 'New Asset'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __d('assets', 'Assets') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
@@ -24,7 +24,7 @@
                         <?= $this->Paginator->sort('modified') ?>
                     </th>
                     <th><?= __d('assets', 'Preview') ?></th>
-                    <th class="actions"><?= __d('assets','Actions') ?></th>
+                    <th class="actions"><?= __d('assets', 'Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
@@ -57,9 +57,9 @@
                         <?= $this->Html->link($asset->getThumbnail() ?: 'Öffnen', ['action' => 'download', $asset->id, '?' => ['download' => $asset->isViewableInBrowser() ? 0 : 1]], ['escape' => false]) ?>
                     </td>
                     <td class="actions">
-                        <?= $this->Html->link(__d('assets','View'), ['action' => 'view', $asset->id]) ?>
-                        <?= $this->Html->link(__d('assets','Edit'), ['action' => 'edit', $asset->id]) ?>
-                        <?= $this->Form->postLink(__d('assets','Delete'), ['action' => 'delete', $asset->id], ['confirm' => __d('assets','Are you sure you want to delete # {0}?', $asset->id)]) ?>
+                        <?= $this->Html->link(__d('assets', 'View'), ['action' => 'view', $asset->id]) ?>
+                        <?= $this->Html->link(__d('assets', 'Edit'), ['action' => 'edit', $asset->id]) ?>
+                        <?= $this->Form->postLink(__d('assets', 'Delete'), ['action' => 'delete', $asset->id], ['confirm' => __d('assets', 'Are you sure you want to delete # {0}?', $asset->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
@@ -68,13 +68,13 @@
     </div>
     <div class="paginator">
         <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __d('assets','first')) ?>
-            <?= $this->Paginator->prev('< ' . __d('assets','previous')) ?>
+            <?= $this->Paginator->first('<< ' . __d('assets', 'first')) ?>
+            <?= $this->Paginator->prev('< ' . __d('assets', 'previous')) ?>
             <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__d('assets','next') . ' >') ?>
-            <?= $this->Paginator->last(__d('assets','last') . ' >>') ?>
+            <?= $this->Paginator->next(__d('assets', 'next') . ' >') ?>
+            <?= $this->Paginator->last(__d('assets', 'last') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->limitControl() ?></p>
-        <p><?= $this->Paginator->counter(__d('assets','Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <p><?= $this->Paginator->counter(__d('assets', 'Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
