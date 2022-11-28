e25115dd090819adaa940ddc7b375c9a02c9e8fd
Pascal Schneider <psc@brandcom.de>
2022-11-28 18:32:15 +0100

    Updated PictureHelper for Cake3

--- src/View/Helper/PictureHelper.php
+++ src/View/Helper/PictureHelper.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Assets\View\Helper;
 
+use Assets\Error\InvalidArgumentException;
+use Assets\Model\Entity\Asset;
 use Assets\Utilities\ImageAsset;
 use Cake\View\Helper;
 
@@ -16,7 +18,7 @@ class PictureHelper extends Helper
     /**
      * Default configuration.
      *
-     * @var array
+     * @var array<string, mixed>
      */
     protected $_defaultConfig = [];
 
@@ -29,13 +31,23 @@ class PictureHelper extends Helper
     /**
      * Returns an html picture element with a webp and jpeg source, and a fallback img element.
      *
-     * @param ImageAsset|string|null $image
-     * -> a path to a static file can be passed.
-     *
-     * @param int[] $widths
-     * @param array $params
+     * @param \Assets\Utilities\ImageAsset|\Assets\Model\Entity\Asset|string|null $image The Asset object, ImageAsset or a path to a static file
+     * @param int[] $widths Array of width which will be present in the SrcSet
+     * @param array $params see info below
      * @return string|null
      * @throws \Exception
+     *
+     * Info about $params:
+     * Will be passed to HtmlHelper::image (e.g. 'alt' or 'title') for the <img> tag.
+     * $params also takes the special parameters 'sizes' and 'filename'.
+     *
+     * 'sizes' will be passed to the <source> tags and should be a valid list of media conditions:
+     * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/source#attr-sizes
+     * https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/sizes
+     * defaults to '100vw'
+     *
+     * 'filename' overrides the automatically generated name of the rendered file, based on the modifications.
+     * It is recommended to leave this empty.
      */
     public function webp($image, array $widths = [300, 500], array $params = []): ?string
     {
@@ -43,58 +55,91 @@ class PictureHelper extends Helper
             return null;
         }
 
+        /**
+         * If a path to a static image, or an Asset is passed, convert it to an ImageAsset
+         */
         if (is_string($image)) {
             $image = ImageAsset::createFromPath($image);
+        } elseif (is_a($image, Asset::class)) {
+            if (!$image->isImage()) {
+                return null;
+            }
+
+            $image = $image->getImage();
         }
 
         if (!is_a($image, ImageAsset::class)) {
-            return null;
+            throw new \http\Exception\InvalidArgumentException(
+                '$image must be passed as an ImageAsset, an Asset which represents an image, or an absolute path (string) to a static image file.'
+            );
         }
 
         $this->image = $image;
         sort($widths);
 
-        $sizes = $params['sizes'] ?? "100vw";
+        $options = [];
+
+        $sizes = $params['sizes'] ?? '100vw';
         unset($params['sizes']);
 
-        return $this->Html->tag('picture',
+        $options['filename'] = $params['filename'] ?? null;
+        $base_filename = $options['filename'] ? $options['filename'] . '-' . current($widths) . 'px' : null;
+        unset($params['filename']);
+
+        return $this->Html->tag(
+            'picture',
             $this->Html->tag('source', null, [
                 'type' => 'image/webp',
-                'srcset' => $this->getSrcSet('webp', $widths),
+                'srcset' => $this->getSrcSet('webp', $widths, $options),
                 'sizes' => $sizes,
             ])
             . $this->Html->tag('source', null, [
                 'type' => 'image/jpeg',
-                'srcset' => $this->getSrcSet('jpeg', $widths),
+                'srcset' => $this->getSrcSet('jpeg', $widths, $options),
                 'sizes' => $sizes,
             ])
-            . $this->image->scaleWidth(current($widths))->toJpg()->getHTML($params)
+            . $this->image->scaleWidth(current($widths))->setFilename($base_filename)->toJpg()->getHTML($params)
         );
     }
 
-    private function getSrcSet(string $format, array $widths): ?string
+    /**
+     * @param string $format Which type of image you want
+     * @param array $widths Array of image widths you desire to have in the SrcSet
+     * @param array $options - passed to PictureHelper::getImageUrl
+     * @return string
+     * @throws \Assets\Error\InvalidArgumentException
+     */
+    private function getSrcSet(string $format, array $widths, array $options = []): string
     {
         $links = [];
 
         foreach ($widths as $width) {
-
             if (!is_int($width)) {
-                throw new \Exception("\$widths must be a list of integers. ");
+                throw new InvalidArgumentException('$widths must be a list of integers.');
             }
 
-            $links[] = $this->getImageUrl($format, $width) . ' ' . $width . 'w';
+            $links[] = $this->getImageUrl($format, $width, $options) . ' ' . $width . 'w';
         }
 
         return implode(', ', $links);
     }
 
-    private function getImageUrl(string $format, int $width): string
+    /**
+     * @param string $format Which type of image you want
+     * @param int $width The width how the image should be output
+     * @param array $options possible options: 'filename'
+     * @return string
+     * @throws \Exception
+     */
+    private function getImageUrl(string $format, int $width, array $options): string
     {
+        $filename = $options['filename'] ? $options['filename'] . '-' . $width . 'px' : null;
+
         switch (strtolower($format)) {
-            case "webp":
-                return $this->image->toWebp()->scaleWidth($width)->getPath();
+            case 'webp':
+                return $this->image->toWebp()->scaleWidth($width)->setFilename($filename)->getPath();
             default:
-                return $this->image->toJpg()->scaleWidth($width)->getPath();
+                return $this->image->toJpg()->scaleWidth($width)->setFilename($filename)->getPath();
         }
     }
 }
